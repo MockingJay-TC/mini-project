65b165d2edc94f12ffad21f1a9bd0e23e278a74e
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-03-16 05:28:09 +0000

    Initial commit

---
+++ src/main/java/classes/Bank.java
@@ -0,0 +1,20 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<>();
+    }
+
+
+    public boolean addBranch(String name){
+        return true;
+    }
+
+}
