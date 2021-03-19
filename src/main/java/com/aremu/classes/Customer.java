2936592044220e1b3cdebdec4edd37124ebae26f
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-03-19 13:46:39 +0000

    comment on customer done

--- src/main/java/com/aremu/classes/Customer.java
+++ src/main/java/com/aremu/classes/Customer.java
@@ -4,16 +4,19 @@ import com.aremu.interfaces.ICustomer;
 
 import java.util.ArrayList;
 
+//The class customer implements ICustomer
 public class Customer implements ICustomer {
     private String name;
     private ArrayList<Double> transactions;
 
 
     public Customer(String name, double initialTransaction) {
+//        condition statement to check if transaction is greater than 0
         initialTransaction = initialTransaction < 0 ? 0 : initialTransaction;
         this.name = name;
     //    instantiate the transactions arrayList
         this.transactions = new ArrayList<>();
+//        adds initialTransaction into the list transactions
         this.transactions.add(initialTransaction);
     }
 
@@ -22,13 +25,16 @@ public class Customer implements ICustomer {
         return this.name;
     }
 
+//    getTransactions method,
     @Override
+//    Return all the list of transactions
     public ArrayList<Double> getTransactions() {
         return this.transactions;
     }
-
+//the addTransactions method
     @Override
     public void addTransactions(double transaction) {
+//        check is transaction is more than 0 before adding it to the list of transactions
         if (transaction > 0) {
             transactions.add(transaction);
         }
